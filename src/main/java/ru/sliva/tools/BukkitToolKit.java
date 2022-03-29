9bc0217bca80fa6a9e0de6b0a272ff23dc269e1f
Geo3gamer <geo3gamer@gmail.com>
2022-03-29 08:04:57 +0300

    Initial commit

---
+++ src/main/java/ru/sliva/tools/BukkitToolKit.java
@@ -0,0 +1,17 @@
+package ru.sliva.tools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BukkitToolKit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
